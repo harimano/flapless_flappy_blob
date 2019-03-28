32db4d994b3f2fd9e24357e20b378abd1f9d5056
Hari Prasath Manokaran <38764929+hari5719@users.noreply.github.com>
2019-03-28 14:34:08 -0700

    working bird

--- sketch.js
+++ sketch.js
@@ -7,5 +7,15 @@ function setup(){
 
 function draw(){
     background(0);
+    bird.update();
     bird.show();
+}
+
+
+function keyPressed(){
+     if(key == ' '){
+         console.log("yeee");
+        bird.up();
+
+     }
 }
